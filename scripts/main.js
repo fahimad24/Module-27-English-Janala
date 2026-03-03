193222a5036328599f549bca02d32c1856dee56e
fahim24 <mdtaraquerahmanfahim@gmail.com>
2026-03-04 01:03:06 +0600

    create modal for showing data details

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+const lessonWordContainer = getId("lesson-word-container");
 const loadLessonBtn = async () => {
     const loadLessonBtn = getId("lesson-container");
     const res = await fetch("https://openapi.programming-hero.com/api/levels/all");
@@ -23,8 +23,9 @@ loadLessonBtn();
 // {id: 5, level: 1, word: 'Eager', meaning: 'আগ্রহী', pronunciation: 'ইগার'}
 
 const loadLessonDetails = async (id) => {
-    const lessonWordContainer = getId("lesson-word-container");
+    // const lessonWordContainer = getId("lesson-word-container");
     const lessonSelecte = getId("lesson-select-container");
+    const lessonEmptyContainer = getId("lesson-empty-container");
     lessonWordContainer.innerHTML = "";
     const res = await fetch(`https://openapi.programming-hero.com/api/level/${id}`);
     const data = await res.json();
@@ -33,9 +34,11 @@ const loadLessonDetails = async (id) => {
 
     if (words.length === 0) {
         lessonWordContainer.classList.add("hidden");
+        lessonEmptyContainer.classList.remove("hidden");
         return;
     } else {
         lessonSelecte.classList.add("hidden");
+        lessonEmptyContainer.classList.add("hidden");
     }
     words.forEach((word) => {
         lessonWordContainer.innerHTML += `
@@ -46,7 +49,7 @@ const loadLessonDetails = async (id) => {
                   <p>Meaning /Pronunciation</p>
                   <h2 class="text-3xl text-center">${word.meaning} /${word.pronunciation}</h2>
                   <div class="flex gap-4 justify-between w-full">
-                    <button class="btn bg-[#badeff4d] mt-4 w-12 h-12 rounded-lg">
+                    <button id="word-details-${word.id}" onclick="showWordDetails(${word.id})" class="btn bg-[#badeff4d] mt-4 w-12 h-12 rounded-lg">
                       <i class="fa-solid fa-circle-info"></i>
                     </button>
                     <button class="btn bg-[#badeff4d] mt-4 w-12 h-12 rounded-lg">
@@ -59,3 +62,38 @@ const loadLessonDetails = async (id) => {
 
     lessonWordContainer.classList.remove("hidden");
 }
+
+const showWordDetails = async (id) => {
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/word/${id}`);
+    const data = await res.json();
+    const wordDetails = data.data;
+    console.log(wordDetails);
+    const wordDetailsContent = getId("word-details-content");
+    wordDetailsContent.innerHTML = `
+        <div class="space-y-4 border-2 border-sky-200 p-3 rounded">
+                <h2 class="text-2xl font-bold">${wordDetails.word} (<i class="fa-solid fa-microphone-lines"></i> : ${wordDetails.pronunciation})</h2>
+                <div class="space-y-2">
+                  <h3 class="text-lg font-semibold">Meaning</h3>
+                  <p class="text-neutral-700">${wordDetails.meaning}</p>
+                </div>
+                <div class="space-y-2">
+                  <h3 class="text-lg font-semibold">Example</h3>
+                  <p class="text-neutral-700">
+                    ${wordDetails.sentence}
+                  </p>
+                </div>
+                <div class="space-y-2">
+                  <h3 class="text-lg font-semibold">Synonyms</h3>
+                  <div>${createElements(wordDetails.synonyms)}</div>
+                </div>
+              </div>
+    `;
+    const myModal = getId("my_modal");
+    myModal.showModal();
+}
+
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+    return htmlElements.join(" ");
+};
